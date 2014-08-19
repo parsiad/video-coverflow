41f6141072b4db465a33331390fb03d6cc9f0351
parsiad <parsiad.azimzadeh@gmail.com>
2014-08-19 16:45:41 -0700

    Removed button_line_edit and consolidated code in browser.py.

--- browser.py
+++ browser.py
@@ -21,7 +21,30 @@ from PySide import QtCore, QtGui, QtOpenGL
 from OpenGL import GLU, GL
 
 from trie import Node, Trie
-from button_line_edit import ButtonLineEdit
+
+# modified from http://stackoverflow.com/questions/12462562/how-to-do-inside-in-qlineedit-insert-the-button-pyqt4
+class ButtonLineEdit(QtGui.QLineEdit):
+    def __init__(self, icon_file, parent=None):
+        super(ButtonLineEdit, self).__init__(parent)
+
+        self.button = QtGui.QToolButton(self)
+        self.button.setIcon(QtGui.QIcon(icon_file))
+        self.button.setStyleSheet('border: 0px; padding: 0px;')
+        self.button.setCursor(QtCore.Qt.ArrowCursor)
+
+        frameWidth = self.style().pixelMetric(QtGui.QStyle.PM_DefaultFrameWidth)
+        buttonSize = self.button.sizeHint()
+
+        self.setStyleSheet('QLineEdit {padding-right: %dpx; }' % (buttonSize.width() + frameWidth + 1))
+        self.setMinimumSize(max(self.minimumSizeHint().width(), buttonSize.width() + frameWidth*2 + 2),
+                            max(self.minimumSizeHint().height(), buttonSize.height() + frameWidth*2 + 2))
+
+    def resizeEvent(self, event):
+        buttonSize = self.button.sizeHint()
+        frameWidth = self.style().pixelMetric(QtGui.QStyle.PM_DefaultFrameWidth)
+        self.button.move(self.rect().right() - frameWidth - buttonSize.width(),
+                         (self.rect().bottom() - buttonSize.height() + 1)/2)
+        super(ButtonLineEdit, self).resizeEvent(event)
 
 # mkdir_p from http://stackoverflow.com/questions/600268/mkdir-p-functionality-in-python
 def mkdir_p(path):
